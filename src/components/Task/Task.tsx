ee92f0cc71a54ea53e6cf08949aee03326263a8c
Andrey Gudin <aeon150791@gmail.com>
2022-11-08 21:47:53 +0400

    feat: add buttons, style for tasks

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { Button, Card, ListGroup } from 'react-bootstrap';
 import { ColumnData } from '../../shared/interfaces';
 
 export const TasksList: FC<ColumnData> = (data) => {
@@ -8,7 +8,13 @@ export const TasksList: FC<ColumnData> = (data) => {
       <ListGroup>
         {data.tasks.map((task) => (
           <ListGroup.Item key={task.id}>
-            <h5>{task.title}</h5>
+            <div className="d-flex align-items-center">
+              <h5>{task.title}</h5>
+              <Button className="ms-auto" variant="link" size="sm">
+                <i className="bi-pencil" style={{ fontSize: '15px', color: 'blue' }}></i>
+              </Button>
+              <button type="button" className="btn-close" aria-label="Close"></button>
+            </div>
             <span>{task.description}</span>
           </ListGroup.Item>
         ))}
