a83e007181ea7ad0601aac3f6d22836ebd186ffb
Andrey_Gudin <aeon150791@gmail.com>
2022-11-14 14:26:39 +0400

    fix: fix spinner animation

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,13 +3,9 @@ import Spinner from 'react-bootstrap/Spinner';
 
 export const SpinnerComponent: FC = () => {
   return (
-    <div>
-      <Spinner
-        animation="border"
-        variant="primary"
-        className="position-fixed top-50 start-50 translate-middle"
-      />
+    <>
+      <Spinner animation="border" variant="primary" className="position-fixed top-50 start-50" />
       <div className="position-fixed top-0 bottom-0 start-0 end-0 bg-secondary opacity-50" />
-    </div>
+    </>
   );
 };
