05e2db873600d3c131462914479c5bdba2e24ed3
Andrey_Gudin <aeon150791@gmail.com>
2022-11-16 10:26:18 +0400

    refactor: change style for headers

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -22,8 +22,11 @@ export const UserProfile: FC = () => {
 
   return (
     <Container className="flex-fill d-flex flex-column gap-2">
-      <h3>Your Name: {users.current?.name}</h3>
-      <h3>Email: {users.current?.login}</h3>
+      <h1>User Profile</h1>
+      <h5>Your Name: </h5>
+      <h3>{users.current?.name}</h3>
+      <h5>Email: </h5>
+      <h3>{users.current?.login}</h3>
       <button className="btn btn-primary align-self-start" onClick={editUserHandler}>
         Edit
       </button>
