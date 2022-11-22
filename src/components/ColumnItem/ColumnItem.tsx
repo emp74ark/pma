11cb7dbd18cc9310e31a09aa461df7f3ebf44c44
emp74ark <andrey.yurkovsky@gmail.com>
2022-11-22 20:52:48 +0100

    feat: add lazy loading of tasks list

--- src/components/ColumnItem/ColumnItem.tsx
+++ src/components/ColumnItem/ColumnItem.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import { Button, ButtonGroup, Card } from 'react-bootstrap';
-import { TasksList } from '../TasksList/TasksList';
+const TasksList = React.lazy(() => import('../TasksList/TasksList'));
 import { Column, ColumnData } from '../../shared/interfaces';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { openModal } from '../../redux/modalSlice';
 import { EditColumn } from '../EditColumn/EditColumn';
+import { SpinnerComponent } from '../Spinner/Spinner';
 
 export const ColumnItem: FC<ColumnData> = (columnData) => {
   const dispatch = useDispatch();
@@ -35,45 +36,47 @@ export const ColumnItem: FC<ColumnData> = (columnData) => {
   };
 
   return (
-    <Card
-      className="flex-grow-0 flex-shrink-0 shadow-sm"
-      style={{ width: '20rem', height: 'fit-content' }}
-      bg={theme}
-      text={theme === 'dark' ? 'white' : 'dark'}
-    >
-      <Card.Header className="gap-3">
-        <div className="row">
-          {visible.editColumn && data?.id === columnData.column.id ? (
-            <EditColumn />
-          ) : (
-            <Card.Title
-              onClick={(e: React.MouseEvent) => editColumnHandler(e, columnData.column)}
-              className="col"
-            >
-              {columnData.column.title}
-            </Card.Title>
-          )}
-
-          <ButtonGroup className="col-4 float-right" size="sm">
-            <Button
-              className="bi-plus-circle text-primary"
-              variant="link"
-              onClick={(e) => addTaskHandler(e, columnData.column)}
-            />
-            <Button
-              className="bi-trash text-danger"
-              variant="link"
-              onClick={(e) => removeColumnHandler(e, columnData.column)}
-            />
-          </ButtonGroup>
-        </div>
-      </Card.Header>
-      <Card.Body
-        style={{ maxHeight: `${maxHeight - 52}px` }}
-        className="d-flex w-100 h-auto flex-column flex-grow-0 flex-shrink-0 gap-3 overflow-auto"
+    <React.Suspense fallback={<SpinnerComponent />}>
+      <Card
+        className="flex-grow-0 flex-shrink-0 shadow-sm"
+        style={{ width: '20rem', height: 'fit-content' }}
+        bg={theme}
+        text={theme === 'dark' ? 'white' : 'dark'}
       >
-        {<TasksList data={columnData} />}
-      </Card.Body>
-    </Card>
+        <Card.Header className="gap-3">
+          <div className="row">
+            {visible.editColumn && data?.id === columnData.column.id ? (
+              <EditColumn />
+            ) : (
+              <Card.Title
+                onClick={(e: React.MouseEvent) => editColumnHandler(e, columnData.column)}
+                className="col"
+              >
+                {columnData.column.title}
+              </Card.Title>
+            )}
+
+            <ButtonGroup className="col-4 float-right" size="sm">
+              <Button
+                className="bi-plus-circle text-primary"
+                variant="link"
+                onClick={(e) => addTaskHandler(e, columnData.column)}
+              />
+              <Button
+                className="bi-trash text-danger"
+                variant="link"
+                onClick={(e) => removeColumnHandler(e, columnData.column)}
+              />
+            </ButtonGroup>
+          </div>
+        </Card.Header>
+        <Card.Body
+          style={{ maxHeight: `${maxHeight - 52}px` }}
+          className="d-flex w-100 h-auto flex-column flex-grow-0 flex-shrink-0 gap-3 overflow-auto"
+        >
+          {<TasksList data={columnData} />}
+        </Card.Body>
+      </Card>
+    </React.Suspense>
   );
 };
